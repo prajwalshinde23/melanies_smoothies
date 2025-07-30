8267e1215c6a5323891d540ee134dc2431ff6655
prajwalshinde23 <prajwalshinde1454@gmail.com>
2025-07-30 15:28:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,12 @@ if time_to_insert:
     st.success('Your Smoothie is ordered!', icon="✅")
 
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
+
+
 
 
 
